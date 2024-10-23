c04e6cc0c11dc73a8c004aeb838c8447ebbf4985
rduo1009 <robertduong09alt@gmail.com>
2024-10-23 11:04:35 +1100

    🐛 fix: adding workaround so that mypy works in python3

--- aenum/_py2.py
+++ aenum/_py2.py
@@ -2,6 +2,9 @@ from operator import div as _div_
 from inspect import getargspec
 
 def raise_with_traceback(exc, tb):
-    raise exc, None, tb
+    if sys.version_info[0] == 2:
+        exec("raise exc, None, tb")
+    else:
+        raise exc.with_traceback(tb)
 
 __all__ = ['_div_', 'getargspec', 'raise_with_traceback']
